e97b42c0e7f019ff548a9174a94e69f60615b068
LiquidGangster <thomasjpkelly@gmail.com>
2019-03-07 16:51:16 -0500

    New interaction with user added.

--- src/USGSDataInteraction.java
+++ src/USGSDataInteraction.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -78,33 +79,21 @@ class USGSDataView {
 	
     public void displayMenu(){
 
-        StringBuilder menu = new StringBuilder();
+        System.out.println("==================");
+        System.out.println("       Menu       ");
+        System.out.println("==================");
+        System.out.println("1. Count any column");
+        System.out.println("==================");
+        System.out.println("2. Search data?");
+        System.out.println("==================");
+        System.out.println("3. Delete data?");
+        System.out.println("===================");
+        System.out.println("4. Quit");
+        System.out.println("==================");
 
-          menu.append("==================\n");
-          System.out.println("		Menu		\n");
-          System.out.println("==================\n");
-          System.out.println("1. Count any column\n");
-          System.out.println("==================\n");
-          System.out.println("2. Search data?\n");
-          System.out.println("==================\n");
-          System.out.println("3. Delete data?\n");
-          System.out.println("===================\n");
-          System.out.println("4. Quit\n");
-          System.out.println("==================\n");
-
-          System.out.println(menu);
-    }
-
-    public void displayCount(){
-
-    }
-
-    public void displaySearch(){
-
-    }
-
-    public void displayDelete(){
-
+//      StringBuilder menu = new StringBuilder();
+//      menu.append("==================");
+//      System.out.println(menu);
     }
 
 
@@ -144,7 +133,10 @@ class USGSDataView {
 class USGSDataController {
 	USGSDataModel model;
 	USGSDataView view;
-	
+    HashMap<String, Double> value = new HashMap<String, Double>();
+    HashMap<String, Double[]> range = new HashMap<String, Double[]>();
+    HashMap<String, Double[]> column = new HashMap<String, Double[]>();
+
 	public USGSDataController(USGSDataModel model, USGSDataView view) {
 		this.model = model;
 		this.view = view;
@@ -155,30 +147,35 @@ class USGSDataController {
 		Scanner input = new Scanner (System.in);
 		do {
 			view.displayMenu();
+			//TODO Handle faulty input
 			int choice = input.nextInt();
 			input.nextLine();
+
 			
 			switch(choice) {
 			//COUNT
             case 1:
-				view.displayCount();
-                System.out.println("FACE");
-                double [] countArray = countLoop();
-
+                solicitValues("LATITUDE");
+                solicitValues("LONGITUDE");
+                solicitValues("DEPTH");
+                solicitValues("MAG");
+                buildCountQuery();
 				break;
             //SEARCH
 			case 2:
-				view.displaySearch();
-                searchLoop();
-
+                solicitValues("LATITUDE");
+                solicitValues("LONGITUDE");
+                solicitValues("DEPTH");
+                solicitValues("MAG");
+                buildSearchQuery();
 				break;
             //DELETE
 			case 3:
-                view.displayDelete();
-                delLoop();
-
-
-				
+                solicitValues("LATITUDE");
+                solicitValues("LONGITUDE");
+                solicitValues("DEPTH");
+                solicitValues("MAG");
+                buildDeleteQuery();
 				break;
             case 4:
                 System.out.println("Exiting program, good bye!");
@@ -186,9 +183,11 @@ class USGSDataController {
             default:
                 System.out.println("Invalid Choice");
 			}
+			range.clear();
+			value.clear();
+			column.clear();
 			System.out.print("Do you want to perform another action?[y/n] ");
         }while(input.next().equals("y"));
-
 	}
 
 
@@ -345,6 +344,159 @@ class USGSDataController {
 
     }
 
+
+    public void solicitValues(String columnName){
+        Scanner input = new Scanner (System.in);
+        int yesFlag = -1;
+        int rangeOrValue = -1;
+        boolean complete = false;
+
+        System.out.println("###############################################################################");
+        System.out.println("Beginning query building process:");
+        System.out.println("###############################################################################");
+        System.out.println("\n");
+
+        //Longitude Interaction:
+        while(!complete) {
+
+            try {
+                System.out.println("Do you want to search on "+columnName+"? Enter 0 for Yes Enter 1 for No");
+                yesFlag = input.nextInt();
+            } catch (InputMismatchException e) {
+                input.next();
+                System.out.println("Type Mismatch - Please Input Either 1 or 2 or 0");
+                continue;
+            }
+            //User must enter 1 or 2 or 0
+            if(yesFlag > 2 || yesFlag < 1){
+                System.out.println("You Must Input Either 1 or 2 or 0");
+                continue;
+            }
+            //If user wants to search on Longitude...
+            if(yesFlag == 1){
+                try {
+                    System.out.println("Do you want to search on a specific value(1) or a range of values(2) or neither (0)?(Enter 1 or 2 or 0)");
+                    rangeOrValue = input.nextInt();
+                } catch (InputMismatchException e) {
+                    input.next();
+                    System.out.println("Please Input Either 1 or 2 or 0");
+                    continue;
+                }
+                //If user wants to search on a specific value
+                if(rangeOrValue == 1){
+                    double longInput = -1;
+                    try {
+                        System.out.println("Please enter a value between -200 and 200");
+                        longInput = input.nextDouble();
+                    } catch (InputMismatchException e) {
+                        input.next();
+                        System.out.println("You may only input a value between -200 and 200");
+                        continue;
+                    }
+                    if(-200 > longInput || longInput > 200){
+                        System.out.println("You may only input a value between -200 and 200");
+                        continue;
+                    }
+                    //Add user's value to the values hashmap
+                    value.put(columnName, longInput);
+                    complete = true;
+
+                    //If user wants to search between a range of values
+                    }else if(rangeOrValue == 2 ){
+                    double rangeInput1 = -1;
+                    double rangeInput2 = -1;
+                        //Get first value
+                        try {
+                            System.out.println("Please enter a value between -200 and 200");
+                            rangeInput1 = input.nextDouble();
+                        } catch (InputMismatchException e) {
+                            input.next();
+                            System.out.println("You may only input a value between -200 and 200");
+                            continue;
+                        }
+                        if(-200 > rangeInput1 || rangeInput1 > 200){
+                            System.out.println("You may only input a value between -200 and 200");
+                            continue;
+                        }
+                        //Get second value
+                        try {
+                            System.out.println("Please enter a value between -200 and 200");
+                            rangeInput2 = input.nextDouble();
+                        } catch (InputMismatchException e) {
+                            input.next();
+                            System.out.println("You may only input a value between -200 and 200");
+                            continue;
+                        }
+                        if(-200 > rangeInput2 || rangeInput2 > 200){
+                            System.out.println("You may only input a value between -200 and 200");
+                            continue;
+                        }
+                        Double[] longArray = new Double[]{rangeInput1,rangeInput2};
+                        range.put(columnName, longArray);
+                        complete = true;
+
+                    }else if(rangeOrValue == 0) {
+
+                    }else{
+                        System.out.println("Please Input Either 1 or 2 or 0");
+                        continue;
+                }
+
+            }
+            System.out.println("VALUE HASHMAP:");
+            value.forEach((key, value) -> System.out.println(key + ":" + value));
+            System.out.println("RANGE HASHMAP:");
+            range.forEach((key, value) -> System.out.println(key + ":" + value));
+            System.out.println("COLUMN HASHMAP:");
+            column.forEach((key, value) -> System.out.println(key + ":" + value));
+        }
+
+
+
+    }
+
+
+
+    public StringBuilder buildCountQuery(){
+
+	    StringBuilder query = new StringBuilder();
+        //Build Operation line
+	    if(column.size() != 0){
+            System.out.println("###############################################################################");
+	        System.out.println("Returning distinct results, only possible when using count on multiple columns:");
+	        System.out.println("###############################################################################");
+	        query.append("SELECT COUNT (DISTINCT ");
+	    }else{
+	        query.append("SELECT COUNT (*)\n");
+	    }
+        //Append column names to search
+//        (Map.Entry<String, Object> entry : map.entrySet()) {
+//            String key = entry.getKey();
+//            Object value = entry.getValue();
+//        }
+
+        //Append Where Clause
+//        (Map.Entry<String, Object> entry : map.entrySet()) {
+//            String key = entry.getKey();
+//            Object value = entry.getValue();
+//        }
+
+	    return query;
+    }
+
+    public StringBuilder buildSearchQuery(){
+
+        StringBuilder query = new StringBuilder();
+
+        return query;
+    }
+
+    public StringBuilder buildDeleteQuery(){
+
+        StringBuilder query = new StringBuilder();
+
+        return query;
+    }
 
 
 }
